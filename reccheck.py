1a08731efacfe963c2580ab41101d2971c4a2bbc
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-04-19 10:21:14 +0100

    Programme creation and comparison working.

--- reccheck.py
+++ reccheck.py
@@ -4,9 +4,13 @@ from collections import namedtuple
 import json
 import glob
 import os
+from datetime import datetime
+from pytz import timezone
+
+london=timezone('Europe/London')
 
 Service=namedtuple("Service",["service_id","mux_id","name"])
-Programme=namedtuple('Programme',["name","service_id","start","end"])
+Programme=namedtuple('Programme',["title","channel","start","stop"])
 
 def service_from_JSON(service_id,mux_id,json_text):
   values=json.loads(json_text)
@@ -27,3 +31,16 @@ def services_from_path(path):
                       'tvheadend/input/dvb/networks/*/muxes/*')):
     services+=services_from_mux(mux_path)
   return services
+
+def programme_from_JSON(json_text):
+  values=json.loads(json_text)
+  return Programme(values['title']['eng'],values['channel'],
+                   london.localize(datetime.fromtimestamp(values['start'])),
+                   london.localize(datetime.fromtimestamp(values['stop'])))
+
+#TODO: simplify this?
+def is_clashing(prog1,prog2):
+  return prog2.start>=prog1.start and prog2.start<=prog1.stop or\
+         prog2.stop>=prog1.start and prog2.stop<=prog1.stop or\
+         prog1.start>=prog2.start and prog1.start<=prog2.stop or\
+         prog1.stop>=prog2.start and prog1.stop<=prog2.stop
--- test_reccheck.py
+++ test_reccheck.py
@@ -2,7 +2,10 @@
 
 from reccheck import Service,service_from_JSON
 from reccheck import services_from_mux,services_from_path
+from reccheck import Programme,programme_from_JSON,is_clashing
 import unittest
+from datetime import datetime
+from pytz import timezone
 
 class TestService(unittest.TestCase):
   def test_service_from_JSON(self):
@@ -25,6 +28,125 @@ class TestService(unittest.TestCase):
                       Service('s2','m1','BBC Two'),
                       Service('s3','m2','ITV')},
                      set(services))
+
+
+class TestProgramme(unittest.TestCase):
+  def test_programme_from_JSON(self):
+    prog_json=\
+    '{\
+      "start": 1461096000,\
+      "stop": 1461102000,\
+      "channel": "f01fa94c246c4b49492f5f3e0f9150dd",\
+      "channelname": "Film4",\
+      "title": {\
+        "eng": "Locke"\
+      }\
+    }'
+    london=timezone('Europe/London')
+    self.assertEqual(Programme(title="Locke",
+                               channel="f01fa94c246c4b49492f5f3e0f9150dd",
+                               start=london.localize(
+                                  datetime(hour=21,
+                                           day=19,month=4,year=2016)),
+                               stop=london.localize(
+                                  datetime(hour=22,minute=40,
+                                           day=19,month=4, year=2016))),
+                     programme_from_JSON(prog_json))
+
+class TestIsClashhing(unittest.TestCase):
+  def setUp(self):
+    self.london=timezone('Europe/London')
+
+  def test_no_clash_different_day(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=20,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=20,month=4, year=2016)))
+    self.assertFalse(is_clashing(p1,p2))
+
+  def tes_mo_clash_different_times(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=9,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=10,minute=40,
+                                                    day=19,month=4, year=2016)))
+    self.assertFalse(is_clashing(p1,p2))
+
+  def test_clash_equal_times(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    self.assertTrue(is_clashing(p1,p2))
+    
+  def test_clash_p1_contains_p2(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=21,minute=15,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=20,
+                                                    day=19,month=4, year=2016)))
+    self.assertTrue(is_clashing(p1,p2))
+    
+  def test_clash_p2_contains_p1(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,minute=20,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=10,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=21,minute=15,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=20,
+                                                    day=19,month=4, year=2016)))
+    self.assertTrue(is_clashing(p1,p2))
+    
+  def test_clash_p1_first(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=21,minute=15,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=50,
+                                                    day=19,month=4, year=2016)))
+    self.assertTrue(is_clashing(p1,p2))
+    
+  def test_clash_p2_first(self):
+    p1=Programme(title="prog1",channel="c1",
+                 start=self.london.localize(datetime(hour=21,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=40,
+                                                    day=19,month=4, year=2016)))
+    p2=Programme(title="prog2",channel="c2",
+                 start=self.london.localize(datetime(hour=20,minute=15,
+                                                     day=19,month=4,year=2016)),
+                 stop=self.london.localize(datetime(hour=22,minute=20,
+                                                    day=19,month=4, year=2016)))
+    self.assertTrue(is_clashing(p1,p2))
 
 
 if '__main__'==__name__:
